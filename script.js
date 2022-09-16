b1f5c19eabdcdb78cb0b77fbb3eb4506d3ff270d
jamurguia91 <80859057+jamurguia91@users.noreply.github.com>
2022-09-16 14:10:46 -0700

    added JS

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-// my scripts
-
-console.log("hi");
+ // change the count-el in the HTML, to reflect the new count
+ let countEl = document.getElementById("count-el")
+ let saveEl = document.getElementById("save-el")
+ let count = 0 
+ 
+ function increment() {
+   count += 1
+   countEl.innerText = count
+ }
+ 
+ function save() {
+   let countStr = count + " - "
+   saveEl.textContent += countStr
+   countEl.textContent = 0
+   count = 0
+ }
+ 
+ 
